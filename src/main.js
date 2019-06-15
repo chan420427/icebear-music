d3d1fec7474b6fe69beeec848b34f8e82f30a7c0
lvchan <2895800864@qq.com>
2019-06-15 19:14:22 +0800

    完成组件m-header

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-//import router from './router'
 import fastclick from 'fastclick'
-
-
-
+// eslint-disable-next-line
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
-
-
+// eslint-disable-next-line
 
 /* eslint-disable no-new */
 new Vue({
